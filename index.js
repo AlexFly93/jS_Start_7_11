71242a24f61e6d099223ea73972ed62a44b945bf
AlexFly93 <muhabon93@gmail.com>
2022-11-07 12:22:19 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let number1 = 45;
+let number2 = 12;
+
+number1 + number2;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+
+let number = 37;
+number += 6;
+number = ++number;
+number /= 2;
+number -= 3;
+number = --number;
+number = number % 4;
+number = number + "qwerty";
