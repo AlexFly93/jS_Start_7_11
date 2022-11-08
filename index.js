57ac7abae77f7c628e510e782b8d68402fab9cd5
AlexFly93 <muhabon93@gmail.com>
2022-11-08 10:40:00 +0200

    Add && and ||

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-let number1 = 45;
-let number2 = 12;
+//let condition = true;
+//console.log(condition);
 
-number1 + number2;
-console.log(number1 + number2);
-console.log(number1 - number2);
-console.log(number1 * number2);
-console.log(number1 / number2);
-console.log(number1 % number2);
+//let number1 = 12;
+//let number2 = 5;
 
-let number = 37;
-number += 6;
-number++;
-number /= 2;
-number -= 3;
-number--;
-number %= 4;
-number += "qwerty";
-console.log(number);
+//console.log(number1, ' > ', number2, ' = ', number1 > number2)
+//console.log(number1, ' >= ', number2, ' = ', number1 >= number2)
+//console.log(number1, ' < ', number2, ' = ', number1 < number2)
+//console.log(number1, ' <= ', number2, ' = ', number1 <= number2)
+
+//let string1 = 'cat'
+//let string2 = 'dog'
+//console.log(string1, ' > ', string2, ' = ', string1 > string2)
+
+//console.log(true, ' && ', true, ' = ', true && true)
+//console.log(true, ' && ', false, ' = ', true && false)
+//console.log(false, ' && ', true, ' = ', false && true)
+//console.log(false, ' && ', false, ' = ', false && false)
+
+//console.log(true, ' || ', true, ' = ', true || true)
+//console.log(true, ' || ', false, ' = ', true || false)
+//console.log(false, ' || ', true, ' = ', false || true)
+//console.log(false, ' || ', false, ' = ', false || false)
+
+let string = "";
+let number = 12;
+let userName = "Brad";
+let bool = false;
+let object = null;
+let resultRemainder = number % 5;
+let result = string || number
+console.log(string || number);
+console.log(result)
